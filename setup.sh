e67046a60e5e5a6cc74db55b067c38326e64caec
Josh Williams <jshwilliams26@gmail.com>
2018-10-27 17:21:44 -0700

    fixing up setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,22 @@
 # Set up Golang
 wget https://storage.googleapis.com/golang/go1.11.1.linux-amd64.tar.gz
 sudo tar -xvf go1.11.1.linux.amd64.tar.gz
-sudo mv go/usr/local
+sudo mv go /usr/local
 export GOROOT=/usr/local/go
 mkdir -p $HOME/Projects/Go
-export GOPATH=$HOME./Projects/Go
+export GOPATH=$HOME/Projects/Go
 export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+
+# Move to GOPATH
+cd $HOME/Projects/Go
+
+# Clone the git repository here
+git clone -b midpoint-progress https://github.com/supernimbus/capstone.git
 
 # Set up Python
 sudo apt install python
+sudo apt install python-pip
+pip install --upgrade -r requirements.txt
 
 # Set up MongoDB
 sudo apt-get install -y mongodb
@@ -18,5 +26,8 @@ mongod --dbpath $mongo_db_path
 # Start the web goat
 bash ./goats/easy_goat/goat_server.sh
 
+# Start the website
+
+
 # Start the crawler
-bash ./src/crawl.sh
+#bash ./src/crawl.sh
